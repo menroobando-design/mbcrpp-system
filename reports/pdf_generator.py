4dd26f8b26a4cde6931556c1ac8496760d641f9e
MENRO Obando <menro@example.com>
2026-07-13 14:58:18 +0800

    Test new PDF generator

--- reports/pdf_generator.py
+++ reports/pdf_generator.py
@@ -1,33 +1,22 @@
 import os
-from io import BytesIO
-
 from django.conf import settings
-
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 
 
 def generate_report_pdf(report):
 
+    print("===== NEW PDF GENERATOR IS RUNNING =====")
+
     filename = os.path.join(
         settings.MEDIA_ROOT,
         f"Report_{report.id}.pdf"
     )
 
-    c = canvas.Canvas(
-        filename,
-        pagesize=A4
-    )
+    c = canvas.Canvas(filename, pagesize=A4)
 
-    width, height = A4
-
-    c.setTitle(f"Report {report.id}")
-
-    c.drawString(
-        50,
-        height - 50,
-        "MBCRPP REPORT"
-    )
+    c.setFont("Helvetica-Bold", 20)
+    c.drawString(100, 800, "THIS IS THE NEW PDF GENERATOR")
 
     c.save()
 
