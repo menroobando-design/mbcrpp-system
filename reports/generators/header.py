64b5e540bda786a1d13075d5cd08de3acdd3eee2
MENRO Obando <menro@example.com>
2026-07-15 07:30:47 +0800

    Adjust PDF layout

--- reports/generators/header.py
+++ reports/generators/header.py
@@ -113,7 +113,7 @@ def draw_header(c, report):
     c.setFont("Helvetica-Oblique", 8)
 
     c.drawCentredString(
-        2 * cm,
+        3.5 * cm,
         height - 4.4 * cm,
         "(Form 1 Barangay)"
     )
@@ -129,7 +129,7 @@ def draw_header(c, report):
     c.setFont("Helvetica", 10)
 
     c.drawString(
-        9.05 * cm,
+        9 * cm,
         height - 5.7 * cm,
         f"Date : {report.activity_date.strftime('%B %d, %Y')}"
     )    
