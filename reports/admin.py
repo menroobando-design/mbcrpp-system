5e6a2d4438db0cfe2b2cf618570f0e3742c19b44
MENRO Obando <menro@example.com>
2026-07-30 07:12:37 +0800

    Register DCFReport in admin

--- reports/admin.py
+++ reports/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import WeeklyReport, ReportPhoto
+from .models import (
+    WeeklyReport,
+    ReportPhoto,
+    DCFReport,
+)
 
 admin.site.register(WeeklyReport)
 admin.site.register(ReportPhoto)
+admin.site.register(DCFReport)
